b15fb3399f024c4449d1f2b47201974ef2fe7956
ultimatixarup <arupkumar.banerjee@gmail.com>
2018-07-22 16:13:18 -0500

    update

---
+++ src/pages/createnode/createnode.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CreatenodePage } from './createnode';
+
+@NgModule({
+  declarations: [
+    CreatenodePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CreatenodePage),
+  ],
+})
+export class CreatenodePageModule {}
